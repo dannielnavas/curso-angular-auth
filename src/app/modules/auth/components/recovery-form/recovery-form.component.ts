6dd64d3be64f8ac1cc89dc2cfe84b66502f23c46
DannielNavas <dannielnavas@gmail.com>
2024-02-28 07:51:18 -0500

    ✨ Añadir funcionalidad de recuperación de contraseña en el componente RecoveryFormComponent

--- src/app/modules/auth/components/recovery-form/recovery-form.component.ts
+++ src/app/modules/auth/components/recovery-form/recovery-form.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { RequestStatus } from '@models/request-status.model';
 
+import { ActivatedRoute, Router } from '@angular/router';
 import { CustomValidators } from '@utils/validators';
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-recovery-form',
@@ -20,16 +23,37 @@ export class RecoveryFormComponent {
       ],
     }
   );
-  status: string = 'init';
+  status: RequestStatus = 'init';
   faEye = faEye;
   faEyeSlash = faEyeSlash;
   showPassword = false;
+  token!: string;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private route: ActivatedRoute, private router: Router) {
+    this.route.queryParams.subscribe((params) => {
+      const { token } = params;
+      if (token) {
+        this.token = token;
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
   recovery() {
     if (this.form.valid) {
-      // Todo
+      this.status = 'loading';
+      const { newPassword } = this.form.getRawValue();
+
+      this.authService.changePassword(newPassword, this.token).subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/login']);
+        },
+        error: () => {
+          this.status = 'failed';
+        },
+      });
     } else {
       this.form.markAllAsTouched();
     }
