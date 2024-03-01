a734a5de96e05295fac282cae7ff42753c082a5a
Danniel Navas <dannielnavas@gmail.com>
2024-03-01 07:28:22 -0500

    Añadir funcionalidad de inicio de sesión y registro en AuthService

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,27 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { ResponseLogin } from '@models/auth.model';
+import { switchMap, tap } from 'rxjs';
+import { TokenService } from './token.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   login(email: string, password: string) {
-    return this.http.post(`${environment.API_URL}/api/v1/auth/login`, {
-      email,
-      password,
-    });
+    return this.http
+      .post<ResponseLogin>(`${environment.API_URL}/api/v1/auth/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.tokenService.saveToken(response.access_token);
+        })
+      );
   }
 
   register(name: string, password: string, email: string) {
-    return this.http.post(`${environment.API_URL}/api/v1/auth/register`, {
-      name,
-      email,
-      password,
-    });
+    return this.http
+      .post(`${environment.API_URL}/api/v1/auth/register`, {
+        name,
+        email,
+        password,
+      })
+      .pipe(switchMap(() => this.login(email, password)));
   }
 
   recovery(email: string) {
@@ -29,9 +39,12 @@ export class AuthService {
   }
 
   changePassword(password: string, token: string) {
-    return this.http.post(`${environment.API_URL}/api/v1/auth/change-password`, {
-      password,
-      token,
-    });
+    return this.http.post(
+      `${environment.API_URL}/api/v1/auth/change-password`,
+      {
+        password,
+        token,
+      }
+    );
   }
 }
