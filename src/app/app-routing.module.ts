976a12a33a02a74fc2ea35b953c0efb5a11066c9
Danniel Navas <dannielnavas@gmail.com>
2024-03-01 07:45:32 -0500

    ✨ Agregada guardia de redirección en la ruta principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guard/auth.guard';
+import { RedirectGuard } from './guard/redirect.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
       import('./modules/auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [RedirectGuard],
   },
   {
     path: 'app',
