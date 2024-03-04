03ea95eaa0eeb9a447239c3a6b7089bc0a936d51
Danniel Navas <dannielnavas@gmail.com>
2024-03-04 07:53:47 -0500

    ✨ Agregado botón de actualización en la tabla de usuarios

--- src/app/modules/users/pages/users-table/users-table.component.ts
+++ src/app/modules/users/pages/users-table/users-table.component.ts
@@ -4,10 +4,9 @@ import { DataSourceUser } from './data-source';
 
 @Component({
   selector: 'app-users-table',
-  templateUrl: './users-table.component.html'
+  templateUrl: './users-table.component.html',
 })
-export class UsersTableComponent  {
-
+export class UsersTableComponent {
   dataSource = new DataSourceUser();
   columns: string[] = ['id', 'avatar', 'name', 'email'];
 
@@ -17,21 +16,24 @@ export class UsersTableComponent  {
         id: 1,
         name: 'User 1',
         email: 'mail@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+        avatar: 'https://api.lorem.space/image/face?w=150&h=150',
       },
       {
         id: 2,
         name: 'User 2',
         email: 'mail2@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
+        avatar: 'https://api.lorem.space/image/face?w=150&h=150',
       },
       {
         id: 3,
         name: 'User 3',
         email: 'mail3@mail.com',
-        avatar: 'https://api.lorem.space/image/face?w=150&h=150'
-      }
+        avatar: 'https://api.lorem.space/image/face?w=150&h=150',
+      },
     ]);
   }
 
+  getUsers() {
+    // CODE
+  }
 }
