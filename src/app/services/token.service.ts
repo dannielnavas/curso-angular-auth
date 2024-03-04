b4ee011386e85ab6178a2ee78f3a9acf8cdfcc67
Danniel Navas <dannielnavas@gmail.com>
2024-03-04 07:34:11 -0500

    ✨ Agregada función isValidToken() al servicio TokenService

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { jwtDecode, JwtPayload } from 'jwt-decode';
 import { getCookie, removeCookie, setCookie } from 'typescript-cookie';
 
 @Injectable({
@@ -15,5 +16,20 @@ export class TokenService {
 
   removeToken() {
     removeCookie('token');
+  }
+
+  isValidToken() {
+    const token = this.getToken();
+    if (!token) {
+      return false;
+    }
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    if (decodedToken && decodedToken?.exp) {
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodedToken.exp);
+      const today = new Date();
+      return tokenDate.getDate() > today.getDate();
+    }
+    return false;
   }
 }
