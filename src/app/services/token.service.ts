4d4bf14ce94a787871e6a581cbb181cfb45a0f3b
Danniel Navas <dannielnavas@gmail.com>
2024-03-04 07:54:00 -0500

    ✨ Agregadas funciones para guardar, obtener y eliminar el token de actualización

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -32,4 +32,31 @@ export class TokenService {
     }
     return false;
   }
+
+  saveRefreshToken(token: string) {
+    setCookie('refresh-token', token, { expires: 1, path: '/' });
+  }
+
+  getRefreshToken() {
+    return getCookie('refresh-token');
+  }
+
+  removeRefreshToken() {
+    removeCookie('refresh-token');
+  }
+
+  isValidRefreshToken() {
+    const token = this.getRefreshToken();
+    if (!token) {
+      return false;
+    }
+    const decodedToken = jwtDecode<JwtPayload>(token);
+    if (decodedToken && decodedToken?.exp) {
+      const tokenDate = new Date(0);
+      tokenDate.setUTCSeconds(decodedToken.exp);
+      const today = new Date();
+      return tokenDate.getDate() > today.getDate();
+    }
+    return false;
+  }
 }
